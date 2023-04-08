00c3acf1adf2d38225609b485f0a2692c04fe380
getulio.souza <getulio.souza@evolutix.com.br>
2023-04-08 15:42:29 -0300

    feature: creating-folder-sctructure

---
+++ src/Components/professional/professional.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function professional() {
+  return (
+    <div>professional</div>
+  )
+}
+
+export default professional
---
+++ src/Pages/viewOrder.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function viewOrder() {
+  return (
+    <>
+    <about />
+    <professional/>
+    <hobbies />
+    </>
+  )
+}
+
+export default viewOrder
