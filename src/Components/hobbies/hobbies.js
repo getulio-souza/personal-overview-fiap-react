baa6b49111976f1e9b3ee9f9cfbd6ec233f12ae6
getulio.souza <getulio.souza@evolutix.com.br>
2023-04-08 23:36:33 -0300

    hobbies-done

--- src/Components/hobbies/hobbies.js
+++ src/Components/hobbies/hobbies.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import {MainSection, MainTitle, ImgContainer} from '../../Assets/global/global'
+import {HobbiesContainer, HobbiesBox, HobbieText, HobbiesImg } from './hobbies.styles'
+import { InfoContainer } from '../about-me/about.styles'
+
+import aboutImg from '../../Assets/images/show-heavy-metal-rock.jpg'
+import show from '../../Assets/images/concert.png'
+import rockFriends from '../../Assets/images/rock-2.png'
+import jam from '../../Assets/images/star.png'
 
 function hobbies() {
   return (
-    <div>hobbies</div>
+    <>
+      <MainSection>
+        <ImgContainer src={aboutImg}></ImgContainer>
+        <InfoContainer>
+        <MainTitle>Meus hobbies</MainTitle>
+        <HobbiesContainer>
+            <HobbiesBox>
+              <HobbiesImg src={show} ></HobbiesImg>
+            <HobbieText>Ir em shows de rock/metal está entre uma das coisas que mais gosto fazer no meu tempo livre.</HobbieText>
+            </HobbiesBox>
+            <HobbiesBox>
+              <HobbiesImg src={rockFriends}></HobbiesImg>
+            <HobbieText>São nesses momentos que me encontro com pessoas que compartilham da mesma paixão pela música que eu.</HobbieText>
+          </HobbiesBox>
+            <HobbiesBox>
+              <HobbiesImg src={jam} ></HobbiesImg>
+            <HobbieText>Assim que um show de uma banda que gosto termina, volto mais feliz e revigorado para casa.</HobbieText>
+          </HobbiesBox>
+        </HobbiesContainer>
+      </InfoContainer>
+      </MainSection>
+    </>
   )
 }
 
---
+++ src/Components/hobbies/hobbies.styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const HobbiesContainer = styled.div`
+    display: flex;
+    padding-top: 40px;
+    justify-content: space-between;
+    align-items: baseline;
+    gap: 20px;
+    text-align: center;
+    text-shadow: rgb(46, 44, 44) 1px 2px 3px;
+`;
+
+export const HobbiesBox = styled.div`
+    background-color: #2a2a2a;
+    padding:15px;
+    border-radius:15px;
+    min-height:200px;
+    min-width:200px;
+    box-shadow: rgb(26, 23, 23) 1px 2px 3px;
+`;
+
+export const HobbieText = styled.div`
+   font-size:14px;
+    padding-top:10px;
+`;
+
+export const HobbiesImg = styled.img `
+   width:50px;
+`;
+
+
+
